774d6613f224dfad85683381161cbe67b0fa3019
josin <josin.madrigal@ucr.ac.cr>
2017-11-29 15:27:29 -0600

    Initial commit

---
+++ app/src/main/java/cr/ac/ucr/ecci/cql/appdemokotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package cr.ac.ucr.ecci.cql.appdemokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
